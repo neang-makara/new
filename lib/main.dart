f392c8959483b14063cd86f2d24825b9fffb0993
neang-makara <makaraneang100@gmail.com>
2020-11-10 11:11:47 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool chang = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          //Images
+          Container(
+          child: chang?Image.asset('assets/images/1.Beutiful.jpeg'):Image.asset('assets/images/2.Beutiful.jpeg')
+          ),
+          //Button
+          RaisedButton(
+            color: Colors.blue,
+              child: Text("Change",style: TextStyle(color: Colors.white,fontSize: 20),),
+              onPressed: (){
+              setState(() {
+                chang =! chang;
+                print('$chang');
+              });
+          }),
+        ],
+      ),
+    );
+  }
+}
+
